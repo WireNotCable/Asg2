cec7e7a3f0c954275c0730d7150a09f132deaac2
lkn120305 <s10242576@connect.np.edu.sg>
2023-01-27 13:32:34 +0800

    Added About Us page
    - Added map to show locations of our stores

---
+++ maps.js
@@ -0,0 +1,28 @@
+let map, searchManager;
+
+function getMap(){
+    map = new Microsoft.Maps.Map('#map', {
+    credentials: 'AjWXG6jbiO7ACveVM5dB5CgfG6TLvWbxIDu9fX7zYwZxwMcCgOqfM91-hnq4_Oix',
+    });
+
+    var location_1 = new Microsoft.Maps.Location(1.3295141648280284, 103.67755912597347)
+
+    var pin_1 = new Microsoft.Maps.Pushpin(location_1, {
+        title: 'Shop'
+    });
+    map.entities.push(pin_1);
+
+    var location_2 = new Microsoft.Maps.Location(1.2996485152337425, 103.80396932929241)
+
+    var pin_2 = new Microsoft.Maps.Pushpin(location_2, {
+        title: 'Shop'
+    });
+    map.entities.push(pin_2);
+
+    var location_3 = new Microsoft.Maps.Location(1.3717093159107367, 103.84839598153981)
+
+    var pin_3 = new Microsoft.Maps.Pushpin(location_3, {
+        title: 'Shop'
+    });
+    map.entities.push(pin_3);
+};
