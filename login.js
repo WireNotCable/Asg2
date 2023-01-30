1b051640f41cf1e6e6fdd73efe07ed69165e277d
Xin Yin <xinyin322@gmail.com>
2023-01-30 13:59:53 +0800

    Created login

--- login.js
+++ login.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     
     // Submit sign up form
     $("#signup-btn").on("click", function(e){
-        //e.preventDefault(); //dont submit first
+        e.preventDefault(); //dont submit first
 
         //get data and POST
         var Username = $("#signup-username").val();
@@ -41,4 +41,43 @@ $(document).ready(function(){
         });
     })
     
+    $("#login-btn").on("click", function(e){
+        e.preventDefault();
+        
+        var responseList = [];
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://idasg2-ba66.restdb.io/rest/signup",
+            "method": "GET",
+            "headers": {
+              "content-type": "application/json",
+              "x-apikey": APIKEY,
+              "cache-control": "no-cache"
+            }
+          }
+          
+          $.ajax(settings).done(function (response) {
+            for (var i=0; i < response.length; i++){
+              responseList.push(new User(response[i].username, response[i].password));
+            }
+
+            var Username = $("#login-username").val();
+            var Password = $("#login-password").val();  
+
+            for (var i=0; i < responseList.length; i++){
+
+              if(responseList[i].username == Username && responseList[i].password == Password){
+                alert("Login successful.");
+                window.location.assign("index.html"); 
+              }
+            }
+          });
+          
+          function User(username, password){
+            this.username = username;
+            this.password = password;
+          }
+    })
 });
