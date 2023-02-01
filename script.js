da2a3583df28c48d093b1c4c84e816210e5a4b06
Xin Yin <xinyin322@gmail.com>
2023-02-01 14:37:42 +0800

    - Removed categories page - Add responsive log in page

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-var searchForm = document.querySelector(".search-form");
-document.querySelector('#search-btn').addEventListener("click", function(){
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
+$(document).ready(function(){
+    $('.navbar-toggler').click(function(){
+        $('.navbar-collapse').slideToggle(300);
+    });
+    
+    smallScreenMenu();
+    let temp;
+    function resizeEnd(){
+        smallScreenMenu();
+    }
+
+    $(window).resize(function(){
+        clearTimeout(temp);
+        temp = setTimeout(resizeEnd, 100);
+        resetMenu();
+    });
 });
 
-var navbar = document.querySelector('.navbar');
-document.querySelector('#menu-btn').addEventListener("click", function(){
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-});
 
-window.onscroll = function() {
-    searchForm.classList.remove('active');
-    navbar.classList.remove('active');
+const subMenus = $('.sub-menu');
+const menuLinks = $('.menu-link');
+
+function smallScreenMenu(){
+    if($(window).innerWidth() <= 992){
+        menuLinks.each(function(item){
+            $(this).click(function(){
+                $(this).next().slideToggle();
+            });
+        });
+    } else {
+        menuLinks.each(function(item){
+            $(this).off('click');
+        });
+    }
+}
+
+function resetMenu(){
+    if($(window).innerWidth() > 992){
+        subMenus.each(function(item){
+            $(this).css('display', 'none');
+        });
+    }
 }
