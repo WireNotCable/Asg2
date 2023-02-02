be984b6c05de2793d011898b9f4aee0585cb411b
lkn120305 <s10242576@connect.np.edu.sg>
2023-02-02 17:58:47 +0800

    Implement swiper.js for Slides Container (Not fully functional)

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-    $('.navbar-toggler').click(function(){
-        $('.navbar-collapse').slideToggle(300);
-    });
-    
-    smallScreenMenu();
-    let temp;
-    function resizeEnd(){
-        smallScreenMenu();
-    }
+  $('.navbar-toggler').click(function(){
+      $('.navbar-collapse').slideToggle(300);
+  });
+  
+  smallScreenMenu();
+  let temp;
+  function resizeEnd(){
+      smallScreenMenu();
+  }
 
-    $(window).resize(function(){
-        clearTimeout(temp);
-        temp = setTimeout(resizeEnd, 100);
-        resetMenu();
-    });
+  $(window).resize(function(){
+      clearTimeout(temp);
+      temp = setTimeout(resizeEnd, 100);
+      resetMenu();
+  });
 });
 
 
@@ -21,25 +21,25 @@ const subMenus = $('.sub-menu');
 const menuLinks = $('.menu-link');
 
 function smallScreenMenu(){
-    if($(window).innerWidth() <= 992){
-        menuLinks.each(function(item){
-            $(this).click(function(){
-                $(this).next().slideToggle();
-            });
-        });
-    } else {
-        menuLinks.each(function(item){
-            $(this).off('click');
-        });
-    }
+  if($(window).innerWidth() <= 992){
+      menuLinks.each(function(item){
+          $(this).click(function(){
+              $(this).next().slideToggle();
+          });
+      });
+  } else {
+      menuLinks.each(function(item){
+          $(this).off('click');
+      });
+  }
 }
 
 function resetMenu(){
-    if($(window).innerWidth() > 992){
-        subMenus.each(function(item){
-            $(this).css('display', 'none');
-        });
-    }
+  if($(window).innerWidth() > 992){
+      subMenus.each(function(item){
+          $(this).css('display', 'none');
+      });
+  }
 }
 
 
@@ -48,13 +48,33 @@ let slides = document.querySelectorAll('.home .slides-container .slide');
 let index = 0;
 
 function next(){
-    slides[index].classList.remove('active');
-    index = (index + 1) % slides.length;
-    slides[index].classList.add('active');
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
 }
 
 function prev(){
-    slides[index].classList.remove('active');
-    index = (index - 1 + slides.length) % slides.length;
-    slides[index].classList.add('active');
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
 }
+
+var mySwiper = new Swiper('.swiper-container', {
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+});
