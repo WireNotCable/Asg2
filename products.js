5f345ea1b0762724a3dd9c4d4800f24ed6122cb7
Xin Yin <xinyin322@gmail.com>
2023-02-02 18:29:34 +0800

    revert back to slides container [partially working

--- products.js
+++ products.js
@@ -1,54 +1,113 @@
 $(document).ready(function(){
-    const APIKEY = "63d670813bc6b255ed0c43ff";  
 
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://idasg2-ba66.restdb.io/rest/products",
-        "method": "GET",
-        "headers": {
-            "content-type": "application/json",
-            "x-apikey": APIKEY,
-            "cache-control": "no-cache"
-        }
-    }
-    let content = "";
-    $.ajax(settings).done(function(response){
-        for(var i=0; i < response.length; i++){
-            content = `<div class="box">
-            <div class="icons">
-              <button class="fas fa-shopping-cart"></button>
-              <button href="#" class="fas fa-heart"></button>
-              <button href="#" class="fas fa-eye"></button>
-            </div>
-            <div class="image">
-              <img src=${response[i].imageName}" alt="">
-            </div>
-            <div class="content">
-              <h3>${response[i].itemName}</h3>
-              <div class="price">$${response[i].price}</div>
-              <div class="stars">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="far fa-star"></i>
+  setTimeout(function () {
+    second();
+  }, 0);
+  first();
+
+
+    function first(){
+      const APIKEY = "63d670813bc6b255ed0c43ff";  
+
+      var settings = {
+          "async": true,
+          "crossDomain": true,
+          "url": "https://idasg2-ba66.restdb.io/rest/products",
+          "method": "GET",
+          "headers": {
+              "content-type": "application/json",
+              "x-apikey": APIKEY,
+              "cache-control": "no-cache"
+          }
+      }
+      let content = "";
+      $.ajax(settings).done(function(response){
+          for(var i=0; i < response.length; i++){
+              content = `<div class="box hi">
+              <div class="icons">
+                <button class="fas fa-shopping-cart"></button>
+                <button href="#" class="fas fa-heart"></button>
+                <button href="#" class="fas fa-eye"></button>
               </div>
-            </div>
-          </div>`
-          $(".products-container").append(content);
-        }
-        
-    })
-
-
-
-
-    // event listener for add to cart
-    var addToCartButtons = document.querySelectorAll(".add-to-cart-btn")
+              <div class="image">
+                <img src=${response[i].imageName}" alt="">
+              </div>
+              <div class="content">
+                <h3>${response[i].itemName}</h3>
+                <div class="price">$${response[i].price}</div>
+                <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="far fa-star"></i>
+                </div>
+              </div>
+            </div>`
+            $(".products-container").append(content);
+          }
+          
+      })  
+    }
+    
+    function second(){
+      // event listener for add to cart
+    var boxes = document.querySelectorAll('.hi')
+    console.log(boxes)
+    console.log('jo')
+    var addToCartButtons = document.querySelectorAll(".fa-shopping-cart")
+    console.log(addToCartButtons)
     for (var i = 0; i < addToCartButtons.length; i++){
       var button = addToCartButtons[i]
       button.addEventListener('click',addToCartClicked)
     }
+
+    // // event listener for add to cart
+    // var addToCartButtons = $("fas fa-shopping-cart add-to-cart-btn")
+    // console.log(document.querySelectorAll(".item-box"))
+    // console.log(addToCartButtons)
+    // for (var i = 0; i < addToCartButtons.length; i++){
+    //     var button = addToCartButtons[i]
+    //     button.addEventListener('click',addToCartClicked(e))
+    // }
+
+    // add data of selected items to local storage after user clicked on add to cart button
+    function addToCartClicked(event){
+        console.log("clicked");
+        var button = event.target
+        var shopItem = button.parentElement
+        var title = shopItem.querySelector('.product-description').innerText
+        var price = shopItem.querySelector('.price').innerText
+        var imageSrc = shopItem.querySelector('.imageLink').src
+        console.log(title, price, imageSrc)
+        var quantity = 1
+        let itemsData = []
+        
+        if (localStorage.getItem("itemsData") !== null) {
+        itemsData = JSON.parse(localStorage.getItem("itemsData"));
+        }
+
+        if (itemsData.some((item) => item.title === title)) {
+        alert("Item has been added.")
+        return
+        }
+
+        let newData = new Data(title, price, imageSrc, quantity);
+
+        // add newData to itemsData and store itemsData in local storage
+        itemsData.push(newData);
+        localStorage.setItem("itemsData", JSON.stringify(itemsData));
+    }
+    
+    function Data(title, price, imageSrc, quantity) {
+        this.title = title;
+        this.price = price;
+        this.imageSrc = imageSrc;
+        this.quantity = quantity;
+    }
+    }
+
+
+    
     
 });
--- script.js
+++ script.js
@@ -18,22 +18,66 @@ $(document).ready(function(){
 
 
 
-var mySwiper = new Swiper('.swiper-container', {
-  loop: true,
-  centeredSlides: true,
-  autoplay: {
-    delay: 9500,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    450: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-  },
+
+    $(document).ready(function(){
+      $('.navbar-toggler').click(function(){
+          $('.navbar-collapse').slideToggle(300);
+      });
+      
+      smallScreenMenu();
+      let temp;
+      function resizeEnd(){
+          smallScreenMenu();
+      }
+    
+      $(window).resize(function(){
+          clearTimeout(temp);
+          temp = setTimeout(resizeEnd, 100);
+          resetMenu();
+      });
+    });
+    
+    
+    const subMenus = $('.sub-menu');
+    const menuLinks = $('.menu-link');
+    
+    function smallScreenMenu(){
+      if($(window).innerWidth() <= 992){
+          menuLinks.each(function(item){
+              $(this).click(function(){
+                  $(this).next().slideToggle();
+              });
+          });
+      } else {
+          menuLinks.each(function(item){
+              $(this).off('click');
+          });
+      }
+    }
+    
+    function resetMenu(){
+      if($(window).innerWidth() > 992){
+          subMenus.each(function(item){
+              $(this).css('display', 'none');
+          });
+      }
+    }
+    
+    
+    
+    let slides = document.querySelectorAll('.home .slides-container .slide');
+    let index = 0;
+    
+    function next(){
+      slides[index].classList.remove('active');
+      index = (index + 1) % slides.length;
+      slides[index].classList.add('active');
+    }
+    
+    function prev(){
+      slides[index].classList.remove('active');
+      index = (index - 1 + slides.length) % slides.length;
+      slides[index].classList.add('active');
+    }
+    
 });
