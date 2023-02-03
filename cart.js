3d0c3df279e3664966fe8cfecdac118d21c5cbf8
Xin Yin <xinyin322@gmail.com>
2023-02-03 21:43:49 +0800

    Created js for cart.js with functions
    - Delete cart item
    - Increment cart item quantity
    - Decrement cart item quantity
    - get prices and update total

--- cart.js
+++ cart.js
@@ -20,6 +20,7 @@ $(document).ready(function(){
     });
 
     function getAndParseAllLocalStorage() {
+      $(".product-card").remove()
       let values = [];
       for (let i = 0; i < localStorage.length; i++) {
         let key = localStorage.key(i);
@@ -60,14 +61,84 @@ $(document).ready(function(){
     }
 
     getAndParseAllLocalStorage()
-
-    
+      
+    // Remove item
     var removeCartItemButtons = document.querySelectorAll(".product-close-btn")
     for (var i = 0; i<removeCartItemButtons.length; i++){
-      
+      var button = removeCartItemButtons[i]
+      button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.parentElement.remove()
+        key = buttonClicked.parentElement.previousElementSibling.firstElementChild.innerHTML
+        localStorage.removeItem(key)
+      })
+      updateTotal()
     }
-      
+
+    // Add quantity
+    var AddQuantityButtons = document.querySelectorAll("#increment")
+    for (var i = 0; i < AddQuantityButtons.length; i++){
+      var button = AddQuantityButtons[i]
+      button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        quantity = buttonClicked.parentElement.previousElementSibling.innerHTML
+        console.log(quantity)
+        buttonClicked.parentElement.previousElementSibling.innerHTML = parseInt(quantity) + 1
+        key = buttonClicked.parentElement.parentElement.parentElement.previousElementSibling.innerHTML
+        let value = JSON.parse(localStorage.getItem(key));
+        value.quantity += 1
+        localStorage.setItem(key, JSON.stringify(value));
+        updateTotal()
+      })
     }
+
+    // Minus quantity
+    var MinusQuantityButtons = document.querySelectorAll("#decrement")
+    for (var i = 0; i < MinusQuantityButtons.length; i++){
+      var button = MinusQuantityButtons[i]
+      button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        quantity = buttonClicked.parentElement.nextElementSibling.innerHTML
+        buttonClicked.parentElement.nextElementSibling.innerHTML = parseInt(quantity) - 1
+        key = buttonClicked.parentElement.parentElement.parentElement.previousElementSibling.innerHTML
+        let value = JSON.parse(localStorage.getItem(key));
+        value.quantity -= 1
+        if (value.quantity == 0 || parseInt(quantity)==0){
+          value.quantity = 1
+          buttonClicked.parentElement.nextElementSibling.innerHTML = 1
+        }
+        localStorage.setItem(key, JSON.stringify(value));
+        updateTotal()
+      })
+    }
+
+    // Calculate Total
+    function updateTotal() {
+      var total = 0
+      var subtotal = 0
+      var tax = 0
+      let cartItems = $('.detail .wrapper');
+      console.log(cartItems)
+      for (var i = 0; i < cartItems.length; i++){
+        let itemContainer = cartItems[i];
+        let priceString = itemContainer.querySelector('#price').innerHTML;
+        let price = parseFloat(priceString.replace('$',''))
+        let quantity = parseInt(itemContainer.querySelector('#quantity').innerHTML);
+        subtotal = parseFloat((subtotal + (price * quantity)).toFixed(2))
+        document.querySelector('#subtotal').innerHTML = subtotal
+        
+        tax = subtotal * parseFloat(0.07)
+        document.querySelector('#tax').innerHTML = tax.toFixed(2)
+
+        let shippingFee = 4;
+        document.querySelector('#shipping').innerHTML = shippingFee.toFixed(2)
+
+        total = subtotal + tax + shippingFee
+        document.querySelector('#total').innerHTML = total.toFixed(2)
+      }
+      }  
+    updateTotal()
+    } 
   )
     
 
@@ -75,15 +146,7 @@ $(document).ready(function(){
 
 
 
-    // var removeCartItemButtons = document.querySelectorAll(".product-close-btn")
-    // for (var i = 0; i<removeCartItemButtons.length; i++){
-    //   var button = removeCartItemButtons[i]
-    //   button.addEventListener('click', function(event){
-    //     var buttonClicked = event.target
-    //     buttonClicked.parentElement.parentElement.parentElement.remove()
-    //     updateStorage()
-    //   })
-    // }
+    
 
     // clear local storage and add existing items back
     // function updateStorage(){
