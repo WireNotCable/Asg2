68061f6a8455743efb72e3247f162a33cc26b609
Xin Yin <xinyin322@gmail.com>
2023-02-05 11:43:49 +0800

    implemented some data validation - password match for sign up page - username exist for login page - password is correct for login page

--- login.js
+++ login.js
@@ -1,27 +1,33 @@
+toggleForm()
+function toggleForm(){
+  section = document.querySelector('.section');
+  container = document.querySelector('.container');
+  container.classList.toggle('active');
+}
 $(document).ready(function(){
     const APIKEY = "63d670813bc6b255ed0c43ff";    
     
     // Submit sign up form
     $("#signup-btn").on("click", function(e){
-        e.preventDefault(); //dont submit first
-
-        //get data and POST request
-        var Username = $("#signup-username").val();
-        var Email = $("#signup-email").val();   
-        var Password = $("#signup-password").val();
-        var ConfirmPassword = $("#signup-confirm-password")
-        var Address = $("#signup-address").val();
-        var DOB = $("#signup-DOB").val();
-        if (Password == ConfirmPassword){
-          let jsondata = {
-            username: Username,
-            email: Email,
-            password: Password,
-            points: 100,
-            address: Address,
-            dob: DOB,
-        }
-        var settings = {
+          e.preventDefault(); //dont submit first
+          //get data and POST request
+          var Username = $("#signup-username").val();
+          var Email = $("#signup-email").val();   
+          var Password = $("#signup-password").val();
+          var ConfirmPassword = $("#signup-confirm-password").val()
+          var Address = $("#signup-address").val();
+          var DOB = $("#signup-DOB").val();
+          if (Password == ConfirmPassword){
+            $("#passwordNotSame").css('display','none');
+            let jsondata = {
+              username: Username,
+              email: Email,
+              password: Password,
+              points: 100,
+              address: Address,
+              dob: DOB,
+          }
+          var settings = {
           "async": true,
           "crossDomain": true,
           "url": "https://idasg2-ba66.restdb.io/rest/signup",
@@ -39,18 +45,24 @@ $(document).ready(function(){
           alert("Registration successful.");
           });
         }
+        else{
+          alert("Please enter matching passwords")
+        }
     })
     
     // Submit log in form
     $("#login-btn").on("click", function(e){
         e.preventDefault();        
-        var responseList = [];
+
+        // get value
+        var Username = $("#login-username").val();
+        var Password = $("#login-password").val();  
 
         // GET request
         var settings = {
             "async": true,
             "crossDomain": true,
-            "url": "https://idasg2-ba66.restdb.io/rest/signup",
+            "url": `https://idasg2-ba66.restdb.io/rest/signup?q={"username":"${Username}"}`,
             "method": "GET",
             "headers": {
               "content-type": "application/json",
@@ -60,31 +72,17 @@ $(document).ready(function(){
           }
           
           $.ajax(settings).done(function (response) {
-            for (var i=0; i < response.length; i++){
-              responseList.push(new User(response[i].username, response[i].password, response[i].points));
+            if (response.length === 0){
+              alert("Account does not exist. Please sign up.");
             }
-
-            // get value
-            var Username = $("#login-username").val();
-            var Password = $("#login-password").val();  
-
-            // loop and find if username and password match with any data in the database
-            for (var i=0; i < responseList.length; i++){
-
-              if(responseList[i].username == Username && responseList[i].password == Password){
-                alert("Login successful.");
-                localStorage.setItem("user", JSON.stringify(responseList[i].username))
-                window.location.assign("index.html"); //auto go back to home page
-              }
+            else if (response[0].password != Password){
+              alert("Incorrect password.");
             }
-            // create user object with username and password to store and responseList
-            function User(username, password, points){
-              this.username = username;
-              this.password = password;
-              this.points = points;
-          }
-          });
-          
-          
+            else{
+              alert("Login successful.");
+              localStorage.setItem("user", JSON.stringify(responseList[i].username));
+              window.location.assign("index.html"); //auto go back to home page
+            }
+          });  
     })
   })
